53a4b75460d943dcf12a2de9c378d5785b26c776
joigmz <jose.izam99@gmail.com>
2021-06-07 18:00:38 -0400

    fix problemin links of jobs

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,14 +1,3 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
-});
-
 const filter = document.querySelector(".filter-title-button")
 const categorias = document.querySelector("#example");
 const button_fil = document.querySelector(".button-filter");
