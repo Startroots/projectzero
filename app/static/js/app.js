15e282b85542339b7941b9a05296cda2dbb1e916
joigmz <jose.izam99@gmail.com>
2021-06-08 17:04:25 -0400

    dejando la caga

--- app/static/js/app.js
+++
@@ -1,9 +0,0 @@
-const filter = document.querySelector(".filter-title-button")
-const categorias = document.querySelector("#example");
-const button_fil = document.querySelector(".button-filter");
-
-
-filter.addEventListener("click", () => {
-  categorias.classList.toggle("abrir");
-  button_fil.classList.toggle("abrir");
-});
