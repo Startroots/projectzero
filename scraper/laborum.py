4d3794fde9eb8a0a8c607ac3dec203110dac6e0f
javduzen <53247682+javduzen@users.noreply.github.com>
2021-06-21 17:43:43 -0400

    cambios scraper linkedin y excel

--- scraper/laborum.py
+++ scraper/laborum.py
@@ -16,24 +16,32 @@ no_of_jobs = int(driver.find_element_by_css_selector('h1>span').get_attribute('i
 print("Numero de anuncios: ", no_of_jobs)
 
 #encontrar todos los trabajos
-job_lists = driver.find_element_by_id('listado-avisos')
-#jobs = job_lists.find_elements_by_tag_name('li') # return a list
+#job_lists = driver.find_elements_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]')
+#jobs = job_lists.find_elements_by_xpath('/div') # return a list
 
 """
 /html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div[2]
 /html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div[3]
 /html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div[4]
 /html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div[5]
+
+ultimo boton
+/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div[25]/div[2]/button
+
+boton siguiente
+/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div[25]/button[2]/i
+
+ 
+    
+for i in range(10):
+    job = driver.find_elements_by_xpath(f'/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div[{i+1}]')
+    try:
+        name = job.find_element_by_css_selector('h2').get_attribute('innerText')
+        print(name)
+    except:
+        pass
 """
 
-for job in job_lists:
-    test = job
-    name = test.find_element_by_css_selector('h2').get_attribute('innerText')
-    
-    print(name)
-
-
-
 #ficha aviso derecha
-job_lists = driver.find_element_by_id('listado-avisos')
+#job_lists = driver.find_element_by_id('ficha-Aviso')
 
